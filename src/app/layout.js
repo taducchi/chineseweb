80be1b6223ff51193b996d415b737f01e1571aa5
taducchi673 <96407475+taducchi673@users.noreply.github.com>
2025-12-18 17:34:46 +0700

    Suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from './context/AuthContext'
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,24 +20,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
         return (
                 <AuthProvider>
-                        <html lang="vi">
-                                <head>
-                                        <link rel="icon" href="/favicon.png" sizes="any" />
-                                          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                        <Suspense fallback={<div>Loading...</div>}>
+                                <html lang="vi">
+                                        <head>
+                                                <link rel="icon" href="/favicon.png" sizes="any" />
+                                                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
-                                        <link
-                                                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-                                                rel="stylesheet"
-                                        />
-                                        <link rel="preconnect" href="https://fonts.googleapis.com" />
-                                        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                                        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800;900&family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-                                </head>
-                                <body className={`${inter.className} font-display antialiased`}>
-                                        {children}
-                                </body>
-                        </html>
-                 </AuthProvider>
+                                                <link
+                                                        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+                                                        rel="stylesheet"
+                                                />
+                                                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                                                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                                                <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800;900&family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+                                        </head>
+                                        <body className={`${inter.className} font-display antialiased`}>
+                                                {children}
+                                        </body>
+                                </html>
+                        </Suspense>
+
+                </AuthProvider>
 
         )
 }
